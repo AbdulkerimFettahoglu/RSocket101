cc579e50c185327834cfb903d250b73fcd509fc5
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2021-12-15 21:10:21 +0300

    Modifications to change web service type from request-response to stream were made.

--- rsocket-server/src/main/java/io/pivotal/rsocketserver/RSocketController.java
+++ rsocket-server/src/main/java/io/pivotal/rsocketserver/RSocketController.java
@@ -2,6 +2,10 @@ package io.pivotal.rsocketserver;
 
 import io.pivotal.rsocketserver.data.Message;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
@@ -11,17 +15,18 @@ public class RSocketController {
 	
 	/**
 	 * To send a example message via RSocket CLI use following command:
-	 * java -jar rsc.jar --debug --request --data "{\"reflector\":\"value\"}" --route request-response tcp://localhost:7000
+	 * java -jar rsc.jar --debug --stream --data "{\"reflector\":\"value\"}" --route stream tcp://localhost:7000
 	 * To download rsc.jar: https://github.com/making/rsc/releases
 	 * 
      * @param request
      * @return Message
      */
-    @MessageMapping("request-response")
-    Message requestResponse(final Message request) {
-        log.info("Received request-response request: {}", request);
-        // create a single Message and return it
-        return new Message(request.getReflector() + " reflected.");
+    @MessageMapping("stream")
+    Flux<Message> stream(Message request) {
+        log.info("Received stream request: {}", request);
+        return Flux
+                .interval(Duration.ofSeconds(1))
+                .map(index -> new Message(index.toString()))
+                .log();
     }
-
 }
