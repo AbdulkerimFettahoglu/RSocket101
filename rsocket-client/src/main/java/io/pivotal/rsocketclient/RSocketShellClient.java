97e2e03ac350db55e9ad800b1756f370d8c78aec
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2021-12-15 22:18:08 +0300

    client side changes to implement channel web service

--- rsocket-client/src/main/java/io/pivotal/rsocketclient/RSocketShellClient.java
+++ rsocket-client/src/main/java/io/pivotal/rsocketclient/RSocketShellClient.java
@@ -3,6 +3,12 @@ package io.pivotal.rsocketclient;
 
 import io.pivotal.rsocketclient.data.Message;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Disposable;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.shell.standard.ShellComponent;
@@ -14,6 +20,8 @@ public class RSocketShellClient {
 
 	// Add a global class variable for the RSocketRequester
 	private final RSocketRequester rsocketRequester;
+	
+	private static Disposable disposable;
 
 	// Use an Autowired constructor to customize the RSocketRequester and store a reference to it in the global variable
 	@Autowired
@@ -22,14 +30,19 @@ public class RSocketShellClient {
 	            .connectTcp("localhost", 7000).block();
 	}
 	
-    @ShellMethod("Send one request. One response will be printed.")
-    public void requestResponse() throws InterruptedException {
-        log.info("\nSending one request. Waiting for one response...");
-        Message message = this.rsocketRequester
-                .route("request-response")
-                .data(new Message("message from eclipse client"))
-                .retrieveMono(Message.class)
-                .block();
-        log.info("\nResponse was: {}", message);
-    }
+    @ShellMethod("Stream some settings to the server. Stream of responses will be printed.")
+	public void channel() {
+		Mono<Duration> setting1 = Mono.just(Duration.ofSeconds(1));
+		Mono<Duration> setting2 = Mono.just(Duration.ofSeconds(3)).delayElement(Duration.ofSeconds(5));
+		Mono<Duration> setting3 = Mono.just(Duration.ofSeconds(5)).delayElement(Duration.ofSeconds(15));
+
+		Flux<Duration> settings = Flux.concat(setting1, setting2, setting3)
+				.doOnNext(d -> log.info("\nSending setting for {}-second interval.\n", d.getSeconds()));
+		
+		disposable = this.rsocketRequester
+                .route("channel")
+                .data(settings)
+                .retrieveFlux(Message.class)
+                .subscribe(message -> log.info("Received: {} \n(Type 's' to stop.)", message));
+	}
 }
