e44b3ea58c167e219090811eb58c68fedc251c31
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2021-12-15 21:11:48 +0300

    "stream" method was added to call stream type web service. And "s" method was added to stop "stream" web service.

--- rsocket-client/src/main/java/io/pivotal/rsocketclient/RSocketShellClient.java
+++ rsocket-client/src/main/java/io/pivotal/rsocketclient/RSocketShellClient.java
@@ -3,6 +3,8 @@ package io.pivotal.rsocketclient;
 
 import io.pivotal.rsocketclient.data.Message;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.Disposable;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.shell.standard.ShellComponent;
@@ -14,6 +16,8 @@ public class RSocketShellClient {
 
 	// Add a global class variable for the RSocketRequester
 	private final RSocketRequester rsocketRequester;
+	
+	private static Disposable disposable;
 
 	// Use an Autowired constructor to customize the RSocketRequester and store a reference to it in the global variable
 	@Autowired
@@ -22,14 +26,20 @@ public class RSocketShellClient {
 	            .connectTcp("localhost", 7000).block();
 	}
 	
-    @ShellMethod("Send one request. One response will be printed.")
-    public void requestResponse() throws InterruptedException {
-        log.info("\nSending one request. Waiting for one response...");
-        Message message = this.rsocketRequester
-                .route("request-response")
-                .data(new Message("message from eclipse client"))
-                .retrieveMono(Message.class)
-                .block();
-        log.info("\nResponse was: {}", message);
+    @ShellMethod("Send one request. Many responses (stream) will be printed.")
+    public void stream() {
+        log.info("\nRequest-Stream. Sending one request. Waiting for unlimited responses (Stop process to quit)...");
+        this.disposable = this.rsocketRequester
+                .route("stream")
+                .data(new Message("message from eclipse client (stream)"))
+                .retrieveFlux(Message.class)
+                .subscribe(er -> log.info("Response received: {}", er));
+    }
+    
+    @ShellMethod("Stop streaming messages from the server.")
+    public void s(){
+        if(null != disposable){
+            disposable.dispose();
+        }
     }
 }
