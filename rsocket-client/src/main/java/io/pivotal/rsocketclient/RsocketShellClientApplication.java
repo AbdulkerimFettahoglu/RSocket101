1cd1eaf3c73f5df1293ade24a8f3e296d1d97de3
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2021-12-15 18:25:13 +0300

    initial commit

---
+++ rsocket-client/src/main/java/io/pivotal/rsocketclient/RsocketShellClientApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.rsocketclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsocketShellClientApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(RsocketShellClientApplication.class, args);
+    }
+}
