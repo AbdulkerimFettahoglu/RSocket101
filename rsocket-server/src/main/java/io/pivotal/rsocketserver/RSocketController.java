052cc3c32ec05062fa928984408fa99a294925df
Abdulkerim Fettahoğlu <abdulkerim.fettahoglu@gmail.com>
2021-12-21 20:37:31 +0300

    comment section was changed.

--- rsocket-server/src/main/java/io/pivotal/rsocketserver/RSocketController.java
+++ rsocket-server/src/main/java/io/pivotal/rsocketserver/RSocketController.java
@@ -20,7 +20,7 @@ public class RSocketController {
 	
 	/**
 	 * To send a example message via RSocket CLI use following command:
-	 * java -jar rsc.jar --debug --request --data "{\"reflector\":\"value\"}" --route request-response tcp://localhost:7000
+	 * java -jar rsc.jar --debug --channel --data - --route channel tcp://localhost:7000
 	 * To download rsc.jar: https://github.com/making/rsc/releases
 	 * 
      * @param request
